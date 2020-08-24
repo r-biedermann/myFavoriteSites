e91f7402b9d9061ada732e2d6595c2484f7f9050
Biedermann <RBiedermann@tobit.ag>
2020-08-24 09:58:26 +0200

    :sparkles: get sitenames

--- src/index.js
+++ src/index.js
@@ -4,9 +4,37 @@ import './app.scss';
 const init = async () => {
     try {
         await chayns.ready;
+        getData();
     } catch (err) {
         console.error('No chayns environment found', err);
     }
 };
+
+const $list = document.querySelector('.list');
+
+const getData = () => {
+    fetch('https://chayns1.tobit.com/TappApi/Site/SlitteApp?SearchString=love&Skip=0&Take=50')
+        .then(function (response) {
+            return response.json();
+        }).then(function (json) {
+            console.log('parsed json', json);
+            createList(json.Data);
+        }).catch(function (ex) {
+            console.log('parsing failed', ex);
+        });
+};
+
+function createList(data) {
+    for (const website of data) {
+        const element = document.createElement('DIV');
+        const name = document.createElement('P');
+
+        element.classList.add('listElement');
+        name.innerHTML = website.appstoreName;
+        
+        $list.appendChild(element);
+        element.appendChild(name);
+    }
+}
 
 init();
