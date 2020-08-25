24a78945bbb397a992c0c66c6cfc902ffb42ef61
Biedermann <RBiedermann@tobit.ag>
2020-08-25 11:19:17 +0200

    :art: improve code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+/* eslint-disable camelcase */
+/* eslint-disable no-empty */
+/* eslint-disable no-plusplus */
 // import login from './utils/login';
 import './app.scss';
 
@@ -6,55 +9,46 @@ let searchString = defaultSearch;
 let timeout;
 let hideListItems = true;
 
-const init = async () => {
-    try {
-        await chayns.ready;
-        getData();
-        if (chayns.env.user.isAuthenticated) {
-            setName();
-        }
-        chayns.ui.initAll();
-        const inputs = document.querySelectorAll('.form-item');
-        for (let i = 0; i < inputs.length; i++) {
-            inputs[i].addEventListener('input', checkForText);
-        }
-        document.querySelector('#send').addEventListener('click', sendForm);
-        document.querySelector('.accordion').addEventListener('click', accordionClicked);
-        document.querySelector('#search').addEventListener('input', searchInput);
-        document.querySelector('#toggle').addEventListener('click', toggleList);
-    } catch (err) {
-        console.error('No chayns environment found', err);
-    }
-};
-
 const $list = document.querySelector('.list');
+const $firstName = document.querySelector('#firstName');
+const $firstName_label = document.querySelector('#firstName-label');
+const $lastName = document.querySelector('#lastName');
+const $lastName_label = document.querySelector('#lastName-label');
+const $eMail = document.querySelector('#eMail');
+const $eMail_label = document.querySelector('#eMail-label');
+const $siteName = document.querySelector('#siteName');
+const $siteName_label = document.querySelector('#siteName-label');
+const $search = document.querySelector('#search');
+const $address = document.querySelector('#address');
+const $postcode = document.querySelector('#postcode');
+const $place = document.querySelector('#place');
+const $comment = document.querySelector('#comment');
+const $accordion = document.querySelector('.accordion');
+const $toggle = document.querySelector('#toggle');
 
 const getData = () => {
-    document.querySelector('.accordion').classList.add('hidden');
+    $accordion.classList.add('hidden');
     chayns.showWaitCursor();
     while ($list.firstElementChild !== null) {
         $list.firstElementChild.remove();
     }
     fetch(`https://chayns1.tobit.com/TappApi/Site/SlitteApp?SearchString=${searchString}&Skip=0&Take=60`)
-        .then(function (response) {
-            return response.json();
-        }).then(function (json) {
-            console.log('parsed json', json);
+        .then(response => response.json()).then((json) => {
             createList(json.Data);
-        }).catch(function (ex) {
-            console.log('parsing failed', ex);
+        }).catch(() => {
             chayns.hideWaitCursor();
         })
-        .then(function () {
-            document.querySelector('.accordion').classList.remove('hidden');
+        .then(() => {
+            $accordion.classList.remove('hidden');
             chayns.hideWaitCursor();
+            hideListItems = true;
             toggleList();
         });
 };
 
 const setName = () => {
-    document.querySelector('#firstName').value = chayns.env.user.firstName;
-    document.querySelector('#lastName').value = chayns.env.user.lastName;
+    $firstName.value = chayns.env.user.firstName;
+    $lastName.value = chayns.env.user.lastName;
     checkForText();
 };
 
@@ -64,10 +58,10 @@ const searchInput = () => {
 };
 
 const search = () => {
-    if (document.querySelector('#search').value === '') {
+    if ($search.value === '') {
         searchString = defaultSearch;
     } else {
-        searchString = document.querySelector('#search').value;
+        searchString = $search.value;
     }
     getData();
 };
@@ -78,29 +72,29 @@ const toggleList = () => {
         for (let i = 30; i < items.length; i++) {
             items[i].classList.add('hidden');
         }
-        document.querySelector('#toggle').innerHTML = 'Mehr';
+        $toggle.innerHTML = 'Mehr';
         hideListItems = false;
     } else {
         for (let i = 30; i < items.length; i++) {
             items[i].classList.remove('hidden');
         }
-        document.querySelector('#toggle').innerHTML = 'Weniger';
+        $toggle.innerHTML = 'Weniger';
         hideListItems = true;
     }
 };
 
 function createList(data) {
-    for (const website of data) {
+    for (let i = 0; i < data.length; i++) {
         const element = document.createElement('div');
         const name = document.createElement('p');
         const background = document.createElement('div');
         const img = document.createElement('div');
 
         element.classList.add('listElement');
-        name.innerHTML = website.appstoreName.substr(0, 15);
+        name.innerHTML = data[i].appstoreName.substr(0, 15);
         background.classList.add('background');
-        img.style = `background-image: url(https://sub60.tobit.com/l/${website.locationId}?size=70); z-index: 1000; width: 70px; height: 70px;`;
-        element.addEventListener('click', () => { chayns.openUrlInBrowser(`https://chayns.net/${website.siteId}`); });
+        img.style = `background-image: url(https://sub60.tobit.com/l/${data[i].locationId}?size=70); z-index: 1000; width: 70px; height: 70px; background-size: cover;`;
+        element.addEventListener('click', () => { chayns.openUrlInBrowser(`https://chayns.net/${data[i].siteId}`); });
 
         $list.appendChild(element);
         element.appendChild(background);
@@ -118,33 +112,33 @@ const checkForText = () => {
             divs[i].classList.add('labelRight');
         }
     }
-    if (document.querySelector('#firstName').value === '') {
-        document.querySelector('#firstName').classList.add('wrong');
-        document.querySelector('#firstName-label').classList.add('wrong');
+    if ($firstName.value === '') {
+        $firstName.classList.add('wrong');
+        $firstName_label.classList.add('wrong');
     } else {
-        document.querySelector('#firstName').classList.remove('wrong');
-        document.querySelector('#firstName-label').classList.remove('wrong');
+        $firstName.classList.remove('wrong');
+        $firstName_label.classList.remove('wrong');
     }
-    if (document.querySelector('#lastName').value === '') {
-        document.querySelector('#lastName').classList.add('wrong');
-        document.querySelector('#lastName-label').classList.add('wrong');
+    if ($lastName.value === '') {
+        $lastName.classList.add('wrong');
+        $lastName_label.classList.add('wrong');
     } else {
-        document.querySelector('#lastName').classList.remove('wrong');
-        document.querySelector('#lastName-label').classList.remove('wrong');
+        $lastName.classList.remove('wrong');
+        $lastName_label.classList.remove('wrong');
     }
-    if (document.querySelector('#eMail').value === '') {
-        document.querySelector('#eMail').classList.add('wrong');
-        document.querySelector('#eMail-label').classList.add('wrong');
+    if ($eMail.value === '') {
+        $eMail.classList.add('wrong');
+        $eMail_label.classList.add('wrong');
     } else {
-        document.querySelector('#eMail').classList.remove('wrong');
-        document.querySelector('#eMail-label').classList.remove('wrong');
+        $eMail.classList.remove('wrong');
+        $eMail_label.classList.remove('wrong');
     }
-    if (document.querySelector('#siteName').value === '') {
-        document.querySelector('#siteName').classList.add('wrong');
-        document.querySelector('#siteName-label').classList.add('wrong');
+    if ($siteName.value === '') {
+        $siteName.classList.add('wrong');
+        $siteName_label.classList.add('wrong');
     } else {
-        document.querySelector('#siteName').classList.remove('wrong');
-        document.querySelector('#siteName-label').classList.remove('wrong');
+        $siteName.classList.remove('wrong');
+        $siteName_label.classList.remove('wrong');
     }
 };
 
@@ -156,14 +150,6 @@ function accordionClicked() {
 }
 
 function sendForm() {
-    const $firstName = document.querySelector('#firstName');
-    const $lastName = document.querySelector('#lastName');
-    const $eMail = document.querySelector('#eMail');
-    const $address = document.querySelector('#address');
-    const $postcode = document.querySelector('#postcode');
-    const $place = document.querySelector('#place');
-    const $siteName = document.querySelector('#siteName');
-    const $comment = document.querySelector('#comment');
     let message = '';
     let fullAddress = '';
     if ($address.value !== '' || $postcode.value !== '' || $place.value !== '') {
@@ -188,7 +174,7 @@ function sendForm() {
             {
                 text: message
             }
-        ).then(function (result) {
+        ).then((result) => {
             if (result.ok) {
                 chayns.dialog.alert('', 'Die Nachricht wurde versendet.');
                 $firstName.value = '';
@@ -206,5 +192,27 @@ function sendForm() {
         });
     }
 }
+
+const init = async () => {
+    try {
+        await chayns.ready;
+        getData();
+        if (chayns.env.user.isAuthenticated) {
+            setName();
+        }
+        chayns.ui.initAll();
+        const inputs = document.querySelectorAll('.form-item');
+        for (let i = 0; i < inputs.length; i++) {
+            inputs[i].addEventListener('input', checkForText);
+        }
+        document.querySelector('#send').addEventListener('click', sendForm);
+        $accordion.addEventListener('click', accordionClicked);
+        $search.addEventListener('input', searchInput);
+        $toggle.addEventListener('click', toggleList);
+    } catch (err) {
+        // eslint-disable-next-line no-console
+        console.error(err);
+    }
+};
 
 init();
